6f769a21df768006c2816a70ff4527c9b8c1549f
guozhixing <Zhixing_guo@foxitsoftware.cn>
2018-02-27 18:19:25 +0800

    feat: 大致框架已搭建

---
+++ myserver/app.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+    app = express();
+app.get('/',(req,res) => {
+    res.send('这不是服务器');
+})
+app.listen(3000,()=> {
+    console.log('\n-----------------------------后台服务器已开启于3000端口-----------------------------\n');
+})
---
+++ webapp/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tabbar>
+      <tabbar-item>
+        <!-- <img slot="icon" src="../static/img/sprite"> -->
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem  } from 'vux'
+
+export default {
+  name: 'app',
+  components: {
+    Tabbar,
+    TabbarItem 
+  }
+}
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
